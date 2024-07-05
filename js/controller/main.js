6e64b8b09f81ac55507cd1d4816915f6b283cc92
ajardinesc <angeljardines.dg@gmail.com>
2024-07-04 21:00:36 -0700

    Se activaron las funciones en boton de formulario

---
+++ js/controller/main.js
@@ -0,0 +1,65 @@
+import { servicesProducts } from "../service/product-service.js";
+
+const productContainer =document.querySelector("[data-products]");
+const form = document.querySelector("[data-form]");
+
+function createCard (name, price, image, id) {
+    const card = document.createElement("div");
+    card.classList.add("cards")
+
+    card.innerHTML = `
+        <div class="img-container">
+            <img class="img__producto" src="${image}" alt="${name}">
+        </div>
+
+            <div class="card-container--info">
+                <p>Nombre</p>
+                <div class="card-container--value">
+                    <p>$ ${price} </p>
+                    <button class="delete-button" data-id="${id}">
+                        <img class="img__borrar" src="./assets/trash.png" alt="Eliminar">
+                </div>
+            </div>
+        
+    `;
+
+    const deleteButton = card.querySelector(".delete-button");
+    deleteButton.addEventListener("click", () => handleDelete(id));
+
+    productContainer.appendChild(card);
+    return card;
+}
+
+const render = async () => {
+    try {
+        const listProduct = await servicesProducts.productList();
+           
+            listProduct.forEach(product => {
+                productContainer.appendChild(createCard(
+                        product.name,
+                        product.price,
+                        product.image,
+                        product.id
+                    )
+                )
+            });
+
+        } catch (error) {
+            console.log(error);
+         }
+};
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector("[data-name]").value;
+    const price = document.querySelector("[data-price]").value;
+    const image = document.querySelector("[data-image]").value;
+
+    servicesProducts.createProducts(name, price, image)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+
+});
+
+render();
